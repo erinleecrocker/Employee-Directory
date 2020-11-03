026824ab24a9ae684809f2ba3f7723bdd4491b94
Erin Crocker <erindowns1@me.com>
2020-11-02 21:03:08 -0500

    changed api

--- employee-directory/src/components/DisplayEmployees/DisplayEmployees.jsx
+++ employee-directory/src/components/DisplayEmployees/DisplayEmployees.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './displayEmployeesStyle.css';
 import Employee from '../Employee/Employee'
 
-const DisplayEmployees = () => {
-
-    return (
-      <div className="Display-Employee">
+class DisplayEmployees extends Component {
+    render() {
+        return (
+            <div>
+                 <div className="Display-Employee">
         <table className="table table-striped">
           <thead>
             <tr>
@@ -42,7 +43,58 @@ const DisplayEmployees = () => {
           </tbody>
         </table>
       </div>
-    );
-};
+            </div>
+        );
+    }
+}
 
 export default DisplayEmployees;
+
+// import React from 'react';
+// import './displayEmployeesStyle.css';
+// import Employee from '../Employee/Employee'
+
+// const DisplayEmployees = () => {
+
+//     return (
+    //   <div className="Display-Employee">
+    //     <table className="table table-striped">
+    //       <thead>
+    //         <tr>
+    //           <th scope="col">Image</th>
+    //           <th scope="col">Name</th>
+    //           <th scope="col">Number</th>
+    //           <th scope="col">Email</th>
+    //           <th scope="col">DOB</th>
+    //         </tr>
+    //       </thead>
+    //       <tbody>
+    //           <Employee />
+    //         {/* <tr>
+    //           <th scope="row">1</th>
+    //           <td>Mark</td>
+    //           <td>Otto</td>
+    //           <td>@mdo</td>
+    //           <td>@fat</td>
+    //         </tr> */}
+    //         <tr>
+    //           <th scope="row">Image</th>
+    //           <td>Jacob</td>
+    //           <td>Thornton</td>
+    //           <td>@fat</td>
+    //           <td>@fat</td>
+    //         </tr>
+    //         <tr>
+    //           <th scope="row">Image</th>
+    //           <td>Larry</td>
+    //           <td>the Bird</td>
+    //           <td>@twitter</td>
+    //           <td>@fat</td>
+    //         </tr>
+    //       </tbody>
+    //     </table>
+    //   </div>
+//     );
+// };
+
+// export default DisplayEmployees;
--- employee-directory/utils/api.js
+++ employee-directory/utils/api.js
@@ -1,29 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios from "axios";
 
 const BASE_URL = 'https://dummyapi.io/data/api/';
 const APP_ID = '{5f9ca8cf59a9a6d614b63dea}';
 
-const App = () => {
-
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-        setLoading(true);
-        axios.get(`${BASE_URL}/user`, { headers: { 'app-id': APP_ID } })
-            .then(({ data }) => setData(data))
-            .catch(console.error)
-            .finally(() => setLoading(false));
-            console.log(data);
-    }, []);
-
-    // return (
-    //     <div>
-    //        {loading && "Loading..."}
-    //        {JSON.stringify(data)}
-    //     </div>
-    // );
+export default {
+    search: function () {
+        return axios.get(
+          "https://randomuser.me/api/?inc=picture,name,phone,email,dob&nat=US&results=20"
+        );
+    },
 };
-
-export { data };
