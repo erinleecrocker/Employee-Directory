494ea10114a901c53a3f7b853312a30bd43ca9e4
Erin Crocker <erindowns1@me.com>
2020-11-02 21:03:35 -0500

    removed unnessesary const from api

--- employee-directory/utils/api.js
+++ employee-directory/utils/api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-const BASE_URL = 'https://dummyapi.io/data/api/';
-const APP_ID = '{5f9ca8cf59a9a6d614b63dea}';
-
 export default {
     search: function () {
         return axios.get(
