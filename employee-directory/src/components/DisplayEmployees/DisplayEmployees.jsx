2e389cba8f5fbd1a2744acc48c703e917b93f9c9
Erin Crocker <erindowns1@me.com>
2020-11-03 00:15:31 -0500

    handled the Name sorting, but still having difficulty with the search input

--- employee-directory/src/components/DisplayEmployees/DisplayEmployees.jsx
+++ employee-directory/src/components/DisplayEmployees/DisplayEmployees.jsx
@@ -1,55 +1,76 @@
 import React, { Component } from 'react';
 import './displayEmployeesStyle.css';
 import Employee from '../Employee/Employee';
+import SearchBar from '../SearchBar/SearchBar';
 import API from "../../utils/API";
-import '../SearchBar/SearchBarStyle.css'
 
 class DisplayEmployees extends Component {
    
     // establish state
     state = {
         result: [],
+        filterResult: [],
         search: "",
+        ascending: true,
     };
     // componentDidMount 
     componentDidMount() {
         this.searchEmployees();
     }
+
     // search Employees
     searchEmployees = (query) => {
-        API.search(query).then((res) =>
-            this.setState({
-                result: res.data.results,
-            })
-        );
+        API.search(query)
+        .then((res) => this.setState({ result: res.data.results, filterResult: res.data.results}))
+        .catch(err => console.log(err));
     };
-    // edit Search
-    editSearch = (event) => {
-        this.setState({search: event.target.value})
-    };
+    // handle input change
+    handleInputChange = event => {
+        const value = event.target.value;
+        const name = event.target.name;
 
+        this.filterEmployee(value);
+
+        this.setState({
+          [name] : value
+        });
+    };
+    // search
+    
+    // sort by name on click and return without click
+    handleNameSort = () => {
+        const compare = (a, b) => {
+            if (this.state.ascending) {
+                if (a.name.first > b.name.first) return 1;
+                if (a.name.first < b.name.first) return -1;
+                return 0;
+          } else {
+                if (a.name.first < b.name.first) return 1;
+                if (a.name.first > b.name.first) return -1;
+                return 0;
+          }
+        };
+    
+        const Order = this.state.result.sort(compare);
+        this.setState({
+          ascending: !this.state.ascending,
+          alteredResult: Order,
+        });
+    };
 
     render() {
         return (
             <div>
-                 <div className = "row search-bar">
-                <div className = "col-sm-2">
-                    <form className="form">
-                        <input 
-                        className="form-control mr-sm-2" 
-                        type="search" 
-                        value = {this.state.search} 
-                        onChange = {this.editSearch}
-                        placeholder="Search" 
-                        aria-label="Search" />
-                    </form>
-                </div>
-                </div>
+                <SearchBar 
+                    value={this.state.search}
+                    handleInputChange={this.handleInputChange}
+                    handleFormSubmit={this.handleFormSubmit} 
+                />
                  <div className="Display-Employee">
                     <table className="table table-striped">
                     <thead>
                         <tr>
-                            <th scope="col">Image</th>
+                            <th scope="col" onClick={this.handleNameSort}>Image</th>
                             <th scope="col">Name</th>
                             <th scope="col">Number</th>
                             <th scope="col">Email</th>
@@ -57,7 +78,7 @@ class DisplayEmployees extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {this.state.result.map((employee) => (
+                        {this.state.filterResult.map((employee) => (
                         <Employee
                             picture={employee.picture.thumbnail}
                             name={employee.name}
--- employee-directory/src/components/SearchBar/SearchBar.jsx
+++ employee-directory/src/components/SearchBar/SearchBar.jsx
@@ -1,44 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './SearchBarStyle.css';
 
-class SearchBar extends Component {
-
-    // establish state
-    // state ={
-    //     search: "",
-    // };
-
-    // When this component mounts, search the Employee data for matching information
-    // componentDidMount() {
-    //     this.searchEmployees()
-    // }
-
-    // searchEmployees = query => {
-    //     API.search(query)
-
-    // }
-    // handle the input change
-
-    // 
-
-    
-    render(props) {
-        return (
+const SearchBar = () => {
+    return (
+        <div>
             <div className = "row search-bar">
                 <div className = "col-sm-2">
                     <form className="form">
                         <input 
                         className="form-control mr-sm-2" 
                         type="search" 
-                        value = {props.search} 
-                        onChange = {props.handleInputChange}
                         placeholder="Search" 
                         aria-label="Search" />
                     </form>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default SearchBar;
