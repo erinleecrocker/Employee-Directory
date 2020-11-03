02de822b210dc39b283b4afddc0a11c053662af6
Erin Crocker <erindowns1@me.com>
2020-11-02 21:56:58 -0500

    accomplished ajax call and using props to dynamically populate employee field in the display employees section, also changed employee to a stateless compnent

--- employee-directory/src/components/DisplayEmployees/DisplayEmployees.jsx
+++ employee-directory/src/components/DisplayEmployees/DisplayEmployees.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import './displayEmployeesStyle.css';
-import Employee from '../Employee/Employee'
+import Employee from '../Employee/Employee';
+import SearchBar from '../SearchBar/SearchBar';
+import API from "../../utils/API";
 
 class DisplayEmployees extends Component {
+   
+    // establish state
+    state = {
+        result: [],
+    };
+    // componentDidMount 
+    componentDidMount() {
+        this.searchEmployees();
+    }
+    // search Employees
+    searchEmployees = (query) => {
+        API.search(query).then((res) =>
+            this.setState({
+                result: res.data.results,
+            })
+        );
+    };
+
     render() {
         return (
             <div>
+                
                  <div className="Display-Employee">
         <table className="table table-striped">
           <thead>
@@ -18,28 +39,15 @@ class DisplayEmployees extends Component {
             </tr>
           </thead>
           <tbody>
-              <Employee />
-            {/* <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>@fat</td>
-            </tr> */}
-            <tr>
-              <th scope="row">Image</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">Image</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-              <td>@fat</td>
-            </tr>
+            {this.state.result.map((employee) => (
+              <Employee 
+                picture={employee.picture.thumbnail}
+                name={employee.name}
+                phone={employee.phone}
+                email={employee.email}
+                dob={employee.dob.date.substr(0, 10)}
+                key={employee.dob.date}/>
+            ))}
           </tbody>
         </table>
       </div>
--- employee-directory/src/components/Employee/Employee.jsx
+++ employee-directory/src/components/Employee/Employee.jsx
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './EmployeeStyle.css';
 
-class Employee extends Component {
+const employee = (props) => {
+    return (
+        <tr>
+          <th scope="row"><img src={props.picture} alt="No Image" /></th>
+          <td>{props.name.first + ' ' + props.name.last}</td>
+          <td>{props.phone}</td>
+          <td>{props.email}</td>
+          <td>{props.dob}</td>
+        </tr>
+    );
+};
 
-    state = {
-        image: "",
-        name: "",
-        number: 0,
-        emailAddress: "",
-        DOB: "",
-    };
+export default employee;
+
+
+// import React, { Component } from 'react';
+// import './EmployeeStyle.css';
+
+// class Employee extends Component {
 
     
-    render() {
-        return (
-            <tr>
-              <th scope="row">Image</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>@fat</td>
-            </tr>
-        );
-    }
-}
+//     render(props) {
+//         return (
+//             <tr>
+//               <th scope="row">{props.picture}</th>
+//               <td>{props.name.first + ' ' + props.name.last}</td>
+//               <td>{props.phone}</td>
+//               <td>{props.email}</td>
+//               <td>{props.dob}</td>
+//             </tr>
+//         );
+//     }
+// }
 
-export default Employee;
+// export default Employee;
